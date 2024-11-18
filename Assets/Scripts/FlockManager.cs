a6f93dee68bc3386e0f023547563396afcfcc799
non0n0no <a55120@alunos.ipb.pt>
2024-11-18 05:10:47 +0000

    flocking

---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager FM;
+    public GameObject fishPrefabGreen;
+    public GameObject fishPrefabRed;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos = Vector3.zero;
+
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(1.0f, 5.0f)]
+    public float rotationSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3 (Random.Range(-swimLimits.x, swimLimits.x), 
+                                                                 Random.Range(-swimLimits.y, swimLimits.y), 
+                                                                 Random.Range(-swimLimits.z, swimLimits.z));
+
+            allFish[i] = Instantiate(fishPrefabGreen, pos, Quaternion.identity);
+            allFish[i] = Instantiate(fishPrefabRed, pos, Quaternion.identity);
+        }
+
+        FM = this;
+
+        goalPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                 Random.Range(-swimLimits.y, swimLimits.y),
+                                                                 Random.Range(-swimLimits.z, swimLimits.z));
+        }
+    }
+}
