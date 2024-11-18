fab81144518cba00c8d8c9fad2844f0a9b2017e8
non0n0no <a55120@alunos.ipb.pt>
2024-11-18 04:55:21 +0000

    creating a city crowd

--- Assets/Scripts/AIControl.cs
+++ Assets/Scripts/AIControl.cs
@@ -5,19 +5,32 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour
 {
-    public GameObject goal;
+    GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        float sm = Random.Range(0.5f, 2.0f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance < 1)
+        {
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
